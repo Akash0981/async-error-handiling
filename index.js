f2faa6ab55a379482dc029c4a20fa3e987eba88a
Akash <akashk494233@gmail.com>
2025-01-27 22:35:04 +0530

    removing spaces

--- index.js
+++ index.js
@@ -18,14 +18,11 @@ main()
 async function main() {
   await mongoose.connect('mongodb://127.0.0.1:27017/errorHandiling');
 }
-
-
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({extended: true}));
-
 
 
 app.get("/", (req, res)=>{
